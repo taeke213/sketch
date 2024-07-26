1c00f6bff537bc979cb955dfa121cdc4b1c9d34d
taeke213 <git@github.com:taeke213/steenpapierschaar.git>
2024-07-26 11:00:58 +0200

    second

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let container = document.getElementById("container")
+let butblack = document.getElementById("black")
+let butwhite = document.getElementById("white")
+let butred = document.getElementById("red")
+let butblue = document.getElementById("blue")
+let butyellow = document.getElementById("yellow") 
+
+let color = "black"
 let check = true
 
-for(let num = 0; num < 256; num +=1){
+for(let num = 0; num < 10000; num +=1){
     const box = document.createElement("div")
     box.classList.add("box")
     box.textContent = "d"
     box.addEventListener(("mouseover"),() => {
         if(check == true)
-        box.style.background = "black"
+        box.style.background = color
     })
     container.appendChild(box)
 }
@@ -22,3 +29,19 @@ document.addEventListener('keyup', (event) => {
         check = true
     }
 });
+
+butblack.addEventListener(("click"), () => {
+    color = "black"
+})
+butwhite.addEventListener(("click"), () => {
+    color = "white"
+})
+butred.addEventListener(("click"), () => {
+    color = "red"
+})
+butblue.addEventListener(("click"), () => {
+    color = "blue"
+})
+butyellow.addEventListener(("click"), () => {
+    color = "yellow"
+})
