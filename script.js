cba478126e58bdb6859a09877d14fafc3452a039
taeke213 <git@github.com:taeke213/steenpapierschaar.git>
2024-07-29 19:22:44 +0200

    reset button and better grid

--- script.js
+++ script.js
@@ -4,18 +4,21 @@ let butwhite = document.getElementById("white")
 let butred = document.getElementById("red")
 let butblue = document.getElementById("blue")
 let butyellow = document.getElementById("yellow") 
+let customcol = document.getElementById("customcolor")
+let resetbut = document.getElementById("reset")
 
 let color = "black"
 let check = true
 
-for(let num = 0; num < 10000; num +=1){
+for(let num = 0; num < 4000; num +=1){
     const box = document.createElement("div")
     box.classList.add("box")
     box.textContent = "d"
     box.addEventListener(("mouseover"),() => {
         if(check == true)
         box.style.background = color
-    })
+})
+
     container.appendChild(box)
 }
 
@@ -44,4 +47,12 @@ butblue.addEventListener(("click"), () => {
 })
 butyellow.addEventListener(("click"), () => {
     color = "yellow"
+})
+customcol.addEventListener(("change"), () => {
+    color = document.getElementById("customcolor").value
+})
+resetbut.addEventListener(("mousedown"), () => {
+    let squares = container.querySelectorAll("div")
+    squares.forEach((div) => (div.style.backgroundColor = "white"))
+
 })
