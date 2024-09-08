b83250ee4938e762ffa9981a026f293c4a43586f
taeke213 <165213208+taeke213@users.noreply.github.com>
2024-09-08 16:17:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let check = true
 for(let num = 0; num < 4000; num +=1){
     const box = document.createElement("div")
     box.classList.add("box")
-    box.textContent = "d"
+    box.innerHtml = "&nbsp;"
     box.addEventListener(("mouseover"),() => {
         if(check == true)
         box.style.background = color
